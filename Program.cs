c4a695040c99d318825abc987b0403cd900d8dc0
davidsc21 <194646074+davidsc21@users.noreply.github.com>
2025-07-26 15:14:57 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("BIENVENIDO AL MENU PRINCIPAL DE LOS MEJORES TORNEOS");
+            Console.WriteLine("POR FAVOR SELECCIONE UNA OPCION:\n1.Administrar Torneos\n2.Administrar equipos\n3.Administrar jugadores\n4.Administrar transferencias\n5.Estadisticas\nSalir");
+            int opcion;
+            string? seleccion = Console.ReadLine();
+            if (int.TryParse(seleccion, out opcion))
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Opcion invalida...");
+            }
+        }
+            
+    }
+}
