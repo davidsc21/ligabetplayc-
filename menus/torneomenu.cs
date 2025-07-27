e1a23b3a32603925b461faf48472f945a09c6c9d
davidsc21 <194646074+davidsc21@users.noreply.github.com>
2025-07-27 17:04:27 -0500

    feat: :sparkles: funciones de torneos terminadas
    se terminaron de crear las funciones del apartado de torneos, tales come crear, buscar, eliminar y editar

--- menus/torneomenu.cs
+++ menus/torneomenu.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using liga.Torneos;
@@ -86,7 +87,58 @@ public static class Menutorneo
                     break;
                 case 4:
                     Console.Clear();
-                    Console.WriteLine("hola4");
+                    Console.Write("Ingrese el ID del torneo a editar: ");
+                    idinput = Console.ReadLine();
+                    int opcion;
+                    if (int.TryParse(idinput, out id) && id > 0)
+                    {
+                        Torneo? editartorneo = Registrotorneo.torneosregistrados.FirstOrDefault(t => t.Id == id);
+                        if (editartorneo != null)
+                        {
+                            Console.WriteLine($"\nTorneo encontrado!\nID: {editartorneo.Id}\nNombre: {editartorneo.Name}\nFecha de creacion: {editartorneo.Creacion}\nPremio: ${editartorneo.Prize}\n(Si no desea editar un valor vuelvalo a escribir exactamente igual)");
+                            Console.Write("\nIngrese que desea editar: \n1.ID\n2.Nombre\n3.Premio\n👉 Opción: ");
+                            string? opcioninput = Console.ReadLine();
+                            if (int.TryParse(opcioninput, out opcion))
+                            {
+                                switch (opcion)
+                                {
+                                    case 1:
+                                        Console.Write("\nIngrese el nuevo ID: ");
+                                        editartorneo.Id = Convert.ToInt32(Console.ReadLine());
+                                        Console.WriteLine("ID actualizado exitosamente!");
+                                        Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                                        Console.ReadKey();
+                                        break;
+                                    case 2:
+                                        Console.Write("\nIngrese el nuevo nombre: ");
+                                        editartorneo.Name = Console.ReadLine();
+                                        Console.WriteLine("Nombre actualizado exitosamente!");
+                                        Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                                        Console.ReadKey();
+                                        break;
+                                    case 3:
+                                        Console.Write("\nIngrese el nuevo valor del premio: ");
+                                        editartorneo.Prize = Convert.ToInt32(Console.ReadLine());
+                                        Console.WriteLine("Premio actualizado exitosamente!");
+                                        Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                                        Console.ReadKey();
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Opcion no valida...");
+                                Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                                Console.ReadKey();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Torneo no encontrado...");
+                            Console.WriteLine("\nPresione cualquie tecla para continuar...");
+                            Console.ReadKey();
+                        }
+                    }
                     Console.ReadKey();
                     break;
                 case 5:
